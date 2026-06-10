884e1f7bd63eef0d6d287b57daed70db364060cb
DrWeremoth <81780606+DrWeremoth@users.noreply.github.com>
2026-06-10 20:47:03 +0200

    Start onboardify.ad.psm1 (#81)
    * Skapat funktion
    
    * Skapat tabell för användardata
    
    * Kontroll av AD-data
    
    * Kod för att skapa användare i AD samt generera lösenord
    
    * Adderat kod för att skapa AD-vändare och reducerat onödig kod
    
    * Förfinade koden
    
    * Revert validation changes from AD pull request
    
    ---------
    
    Co-authored-by: Gustafssoon <gabrielgustafsson96@gmail.com>

--- src/modules/Onboardify.AD.psm1
+++ src/modules/Onboardify.AD.psm1
@@ -1,3 +1,37 @@
 ﻿# Ser till att svenska tecken som å, ä och ö visas rätt i terminalen
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $OutputEncoding = [System.Text.Encoding]::UTF8
+
+function New-OnboardifyADUser {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [PSObject]$User
+    )
+
+    #Skapa användarnamn genom att kombinera förnamn och efternamn
+    $username = ($User.firstName.Substring(0,1) + $User.lastName).ToLower()
+
+    #Skapa en tabell med de attribut som skall användas för att skapa användaren i AD
+    $userAttributes = @{
+        "sAMAccountName" = $username
+        "givenName" = $User.firstName
+        "sn" = $User.lastName
+        "displayName" = "$($User.firstName) $($User.lastName)"
+        "mail" = $User.email
+    }
+
+    #Kontrollera att användaren inte redan finns i AD
+    if (Get-ADUser -Filter { sAMAccountName -eq $username }) {
+        Write-Host "Användaren $username finns redan i AD."
+        return
+    }
+    else {
+        #Skapa användaren i AD med randomizerat lösenord med 12 tecken och 2 icke-alfanumeriska tecken.
+        $password = [System.Web.Security.Membership]::GeneratePassword(12, 2)
+        New-ADUser @userAttributes -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -enabled $true
+        Write-Host "Användaren $username har skapats i AD med lösenord: $password"
+        return
+    }
+}
+        
