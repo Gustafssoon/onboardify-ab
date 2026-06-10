2186aa796b045570636fb3ced15a8e511f24147c
DrWeremoth <drweremoth@gmail.com>
2026-06-10 17:19:48 +0200

    Skapat funktion

--- src/modules/Onboardify.AD.psm1
+++ src/modules/Onboardify.AD.psm1
@@ -1,3 +1,18 @@
 ﻿# Ser till att svenska tecken som å, ä och ö visas rätt i terminalen
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $OutputEncoding = [System.Text.Encoding]::UTF8
+
+function New-OnboardifyADUser {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [PSObject]$User,
+
+        [Parameter(Mandatory = $false)]
+        [string]$Path = 'CN=Users,DC=example,DC=com'
+    )
+
+    #Skapa användarnamn genom att kombinera förnamn och efternamn
+    $username = ($User.firstName.Substring(0,1) + $User.lastName).ToLower()
+}
+        
