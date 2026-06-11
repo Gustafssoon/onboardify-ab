cbe65f613c29f7934a21419e43f6f129ddf461b1
DrWeremoth <drweremoth@gmail.com>
2026-06-11 14:54:43 +0200

    try catch för felkontroll

--- src/modules/Onboardify.AD.psm1
+++ src/modules/Onboardify.AD.psm1
@@ -11,30 +11,35 @@ function New-OnboardifyADUser {
     )
 
     #Skapa användarnamn genom att kombinera förnamn och efternamn
-    $username = ($User.firstName.Substring(0,1) + $User.lastName).ToLower()
+    $username = ($User.firstName.Substring(0, 1) + $User.lastName).ToLower()
 
     #Skapa en tabell med de attribut som skall användas för att skapa användaren i AD
     $userAttributes = @{
         "sAMAccountName" = $username
-        "givenName" = $User.firstName
-        "sn" = $User.lastName
-        "displayName" = "$($User.firstName) $($User.lastName)"
-        "mail" = $User.email
-        "Path" = $User.organizationUnit
-        "memberOf" = $User.groups
+        "givenName"      = $User.firstName
+        "sn"             = $User.lastName
+        "displayName"    = "$($User.firstName) $($User.lastName)"
+        "mail"           = $User.email
+        "Path"           = $User.organizationUnit
+        "memberOf"       = $User.groups
     }
 
-    #Kontrollera att användaren inte redan finns i AD
-    if (Get-ADUser -Filter { sAMAccountName -eq $username }) {
-        throw "Användaren $username finns redan i AD."
-        return
+    try {
+        #Kontrollera att användaren inte redan finns i AD
+        if (Get-ADUser -Filter { sAMAccountName -eq $username }) {
+            throw "Användaren $username finns redan i AD."
+        }
+        else {
+            #Skapa användaren i AD med attribut från $userAttributes och randomizerat lösenord med 12 tecken och 2 icke-alfanumeriska tecken.
+            $password = [System.Web.Security.Membership]::GeneratePassword(12, 2)
+            New-ADUser @userAttributes -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -enabled $true
+            Write-Host "Användaren $username har skapats i AD med lösenord: $password"
+        }
+
     }
-    else {
-        #Skapa användaren i AD med attribut från $userAttributes och randomizerat lösenord med 12 tecken och 2 icke-alfanumeriska tecken.
-        $password = [System.Web.Security.Membership]::GeneratePassword(12, 2)
-        New-ADUser @userAttributes -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -enabled $true
-        Write-Host "Användaren $username har skapats i AD med lösenord: $password"
-        return
+    catch {
+        Write-Host "Fel vid skapande av användaren $username: $($_.Exception.Message)" -ForegroundColor Red
+        throw
     }
 }
         
