dac855118c572805522c51b3e9ed2631f49732d6
DrWeremoth <81780606+DrWeremoth@users.noreply.github.com>
2026-06-11 17:30:25 +0200

    Uttökade userAttributes för Issue 17 och 18 + kommentarer. (#87)
    * Uttökade userAttributes för Issue 17 och 18 + kommentarer.
    
    * try catch för felkontroll
    
    * Korrigeringar i syntax och kommentarer.
    
    * fix: använd korrekta AD-parametrar och grupptillägg
    
    Byter AD-attributnamn till New-ADUser-parametrar och flyttar grupptillhörigheter till Add-ADGroupMember efter att användaren skapats.
    
    ---------
    
    Co-authored-by: Gabriel Gustafsson <gabrielgustafsson96@gmail.com>

--- src/modules/Onboardify.AD.psm1
+++ src/modules/Onboardify.AD.psm1
@@ -6,32 +6,50 @@ function New-OnboardifyADUser {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
         [PSObject]$User
     )
 
     #Skapa användarnamn genom att kombinera förnamn och efternamn
-    $username = ($User.firstName.Substring(0,1) + $User.lastName).ToLower()
+    $username = ($User.firstName.Substring(0, 1) + $User.lastName).ToLower()
 
-    #Skapa en tabell med de attribut som skall användas för att skapa användaren i AD
+    # Skapa en tabell med attribut som används för att skapa användaren i AD
     $userAttributes = @{
-        "sAMAccountName" = $username
-        "givenName" = $User.firstName
-        "sn" = $User.lastName
-        "displayName" = "$($User.firstName) $($User.lastName)"
-        "mail" = $User.email
+        SamAccountName = $username
+        GivenName      = $User.firstName
+        Surname        = $User.lastName
+        DisplayName    = "$($User.firstName) $($User.lastName)"
+        EmailAddress   = $User.email
+        Path           = $User.organizationUnit
+        Name           = "$($User.firstName) $($User.lastName)"
     }
 
-    #Kontrollera att användaren inte redan finns i AD
-    if (Get-ADUser -Filter { sAMAccountName -eq $username }) {
-        Write-Host "Användaren $username finns redan i AD."
-        return
-    }
-    else {
-        #Skapa användaren i AD med randomizerat lösenord med 12 tecken och 2 icke-alfanumeriska tecken.
+    try {
+        #Kontrollera om användaren redan finns i AD
+        if (Get-ADUser -Filter { sAMAccountName -eq $username }) {
+            Write-Host "Användaren $username finns redan i AD." -ForegroundColor Yellow
+            return
+        }
+        
+        # Skapa randomiserat lösenord med 12 tecken och 2 icke-alfanumeriska tecken
         $password = [System.Web.Security.Membership]::GeneratePassword(12, 2)
-        New-ADUser @userAttributes -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -enabled $true
-        Write-Host "Användaren $username har skapats i AD med lösenord: $password"
-        return
+
+        # Skapa användaren i AD
+        New-ADUser @userAttributes `
+            -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+            -Enabled $true `
+            -ErrorAction Stop
+
+        # Lägg till användaren i grupper efter att kontot har skapats
+        foreach ($group in $User.groups) {
+            Add-ADGroupMember -Identity $group -Members $username -ErrorAction Stop
+        }
+
+        Write-Host "Användaren $username har skapats i AD med lösenord: $password" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Fel vid skapande av användaren $username: $($_.Exception.Message)" -ForegroundColor Red
+        throw
     }
 }
         
